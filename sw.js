a068bdd03cc6fcc24208b959c54039c0ac7d15fb
kingchemistry202020-ui <kingchemistry202020@gmail.com>
2026-09-19 01:54:59 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+﻿const CACHE = 'nd-cache-v1';
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.open(CACHE).then(c =>
+      fetch(e.request).then(res => {
+        if (res.ok && e.request.url.indexOf(self.location.origin) === 0) c.put(e.request, res.clone());
+        return res;
+      }).catch(() => c.match(e.request).then(m => m || caches.match('./index.html')))
+    )
+  );
+});
